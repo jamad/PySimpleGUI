2d831d66c8651fe550b8d2ca1f2bf8e136d279f1
jamad <jamad2001@gmail.com>
2019-11-09 21:31:54 +0200

    Update Demo_Pong.py
    reusing objects without recreating them

--- DemoPrograms/Demo_Pong.py
+++ DemoPrograms/Demo_Pong.py
@@ -3,57 +3,47 @@ import random, time
 #   Pong Original code: https://www.pygame.org/project/3649/5739
 
 class Ball:
-    def __init__(self, canvas, bat, bat2, color):
+    def __init__(self, canvas, bat, bat2):
         self.C = canvas
         self.bat = bat
         self.bat2 = bat2
-        self.id = self.C.create_oval(10, 10, 35, 35, fill=color)
+        self.id=self.C.create_oval(10, 10, 35, 35, fill='white')
 
-        self.score1 = self.score2 = 0
-        self.drawPlayer1 = self.drawPlayer2= None
-
-        self.C.move(self.id, 327, 220)
         self.x=random.choice([-2.5, 2.5])
         self.y=-2.5
 
+        self.score1 = self.score2 = 0
+        self.scoreText1 = self.C.create_text(550, 50, font=('freesansbold.ttf', 40), text='0', fill='magenta')
+        self.scoreText2= self.C.create_text(170, 50, font=('freesansbold.ttf', 40), text='0', fill='orange')
+
     def checkwin(self):return self.score1 >= 10 and 'Player left'or self.score2 >= 10 and 'Player Right'or None
 
-    def updatep(self, val):
-        self.C.delete(self.drawPlayer2)
-        self.drawPlayer2= self.C.create_text(170, 50, font=('freesansbold.ttf', 40), text=str(val), fill='orange')
-
-    def updatep1(self, val):
-        self.C.delete(self.drawPlayer1)
-        self.drawPlayer1 = self.C.create_text(550, 50, font=('freesansbold.ttf', 40), text=str(val), fill='magenta')
-
-    def hit_bat(self, a,b,c,d):
-        BP = self.C.coords(self.bat.id)
-        if BP[0] <= c and a <= BP[2]:return BP[1] <= d <= BP[3]
-
-    def hit_bat2(self, a,b,c,d):
-        BP = self.C.coords(self.bat2.id)
-        if BP[0] <= c and a <= BP[2]:return BP[1] <= d <= BP[3]
+    def hit_bat(self,i,a,c,d):
+        A,B,C,D = self.C.coords(i)
+        if A <= c and a <= C:return B <= d <= D
 
     def draw(self):
         self.C.move(self.id, self.x, self.y)
         a,b,c,d= self.C.coords(self.id)
-        if b <= 0:self.y = 4
-        if d >= self.C.winfo_height():self.y = -4
-        if a <= 0:
+        if b <= 0:self.y = 4 # wall reflect
+        if self.C.winfo_height() <= d:self.y = -4 # wall reflect
+        if self.hit_bat(self.bat.id,a,c,d):self.x = 4 # player reflect
+        if self.hit_bat(self.bat2.id,a,c,d):self.x = -4 # player reflect
+        if a <= 0:# ball escaped left
             self.score2 += 1
             self.C.move(self.id, 327, 220)
             self.x = 4
-            self.updatep1(self.score2)
-        if c >= self.C.winfo_width():
+#           self.scoreText1.text=str(self.score2) # this doesn't work but the following works
+#           https://stackoverflow.com/questions/55731450/how-to-create-text-on-a-tkinter-canvas-and-change-the-text-in-the-canvas
+            self.C.itemconfigure(self.scoreText1, text=str(self.score2))
+        if c >= self.C.winfo_width():# ball escaped right
             self.score1 += 1
             self.C.move(self.id, -327, -220)
             self.x = -4
-            self.updatep(self.score1)
-        if self.hit_bat(a,b,c,d):self.x = 4
-        if self.hit_bat2(a,b,c,d):self.x = -4
+            self.C.itemconfigure(self.scoreText2, text=str(self.score1))
 
 class pongbat():
-    def __init__(self, canvas, color,x=40,y=25):
+    def __init__(self, canvas, color,x,y):
         self.C = canvas
         self.id = self.C.create_rectangle(x, 200, y, 310, fill=color)
         self.dy = 0
@@ -70,9 +60,10 @@ W = Window('The Classic Game of Pong', L,return_keyboard_events=True, finalize=T
 # ------------- Create line down center, the bats and ball -------------
 C = W['canvas'].TKCanvas
 C.create_line(350, 0, 350, 400, fill='cyan')
-P = pongbat(C, 'orange')
+
+P = pongbat(C, 'orange',40,25)
 Q = pongbat(C, 'magenta',680,660)
-B = Ball(C, P, Q, 'white')
+B = Ball(C, P, Q)
 
 while 1:# ------------- Event Loop -------------
     B.draw()
