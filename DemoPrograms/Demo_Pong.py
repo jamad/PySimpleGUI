a2789b196cae60e75a0dbb06ec7fcfd1b5be36d3
jamad <jamad2001@gmail.com>
2019-11-09 20:18:12 +0200

    Update Demo_Pong.py

--- DemoPrograms/Demo_Pong.py
+++ DemoPrograms/Demo_Pong.py
@@ -4,51 +4,51 @@ import random, time
 
 class Ball:
     def __init__(self, canvas, bat, bat2, color):
-        self.canvas = canvas
+        self.C = canvas
         self.bat = bat
         self.bat2 = bat2
         self.playerScore = 0
         self.player1Score = 0
         self.drawP1 = None
         self.drawP = None
-        self.id = self.canvas.create_oval(10, 10, 35, 35, fill=color)
-        self.canvas.move(self.id, 327, 220)
-        self.canvas_height = self.canvas.winfo_height()
-        self.canvas_width = self.canvas.winfo_width()
+        self.id = self.C.create_oval(10, 10, 35, 35, fill=color)
+        self.C.move(self.id, 327, 220)
+        self.C_height = self.C.winfo_height()
+        self.C_width = self.C.winfo_width()
         self.x = random.choice([-2.5, 2.5])
         self.y = -2.5
 
     def checkwin(self):return self.playerScore >= 10 and 'Player left'or self.player1Score >= 10 and 'Player Right'or None
 
     def updatep(self, val):
-        self.canvas.delete(self.drawP)
-        self.drawP = self.canvas.create_text(170, 50, font=('freesansbold.ttf', 40), text=str(val), fill='white')
+        self.C.delete(self.drawP)
+        self.drawP = self.C.create_text(170, 50, font=('freesansbold.ttf', 40), text=str(val), fill='white')
 
     def updatep1(self, val):
-        self.canvas.delete(self.drawP1)
-        self.drawP1 = self.canvas.create_text(550, 50, font=('freesansbold.ttf', 40), text=str(val), fill='white')
+        self.C.delete(self.drawP1)
+        self.drawP1 = self.C.create_text(550, 50, font=('freesansbold.ttf', 40), text=str(val), fill='white')
 
     def hit_bat(self, pos):
-        bat_pos = self.canvas.coords(self.bat.id)
+        bat_pos = self.C.coords(self.bat.id)
         if pos[2] >= bat_pos[0] and pos[0] <= bat_pos[2]:return pos[3] >= bat_pos[1] and pos[3] <= bat_pos[3]
 
     def hit_bat2(self, pos):
-        bat_pos = self.canvas.coords(self.bat2.id)
+        bat_pos = self.C.coords(self.bat2.id)
         if pos[2] >= bat_pos[0] and pos[0] <= bat_pos[2]:return pos[3] >= bat_pos[1] and pos[3] <= bat_pos[3]
 
     def draw(self):
-        self.canvas.move(self.id, self.x, self.y)
-        pos = self.canvas.coords(self.id)
+        self.C.move(self.id, self.x, self.y)
+        pos = self.C.coords(self.id)
         if pos[1] <= 0:self.y = 4
-        if pos[3] >= self.canvas_height:self.y = -4
+        if pos[3] >= self.C_height:self.y = -4
         if pos[0] <= 0:
             self.player1Score += 1
-            self.canvas.move(self.id, 327, 220)
+            self.C.move(self.id, 327, 220)
             self.x = 4
             self.updatep1(self.player1Score)
-        if pos[2] >= self.canvas_width:
+        if pos[2] >= self.C_width:
             self.playerScore += 1
-            self.canvas.move(self.id, -327, -220)
+            self.C.move(self.id, -327, -220)
             self.x = -4
             self.updatep(self.playerScore)
         if self.hit_bat(pos):self.x = 4
@@ -57,44 +57,42 @@ class Ball:
 
 class pongbat():
     def __init__(self, canvas, color,x=40,y=25):
-        self.canvas = canvas
-        self.id = self.canvas.create_rectangle(x, 200, y, 310, fill=color)
-        self.canvas_height = self.canvas.winfo_height()
-        self.canvas_width = self.canvas.winfo_width()
-        self.y = 0
-    def up(self, evt):self.y = -5
-    def down(self, evt):self.y = 5
+        self.C = canvas
+        self.id = self.C.create_rectangle(x, 200, y, 310, fill=color)
+        self.dy = 0
+    def mv(self,v):self.dy=v
     def draw(self):
-        self.canvas.move(self.id, 0, self.y)
-        pos = self.canvas.coords(self.id)
-        if pos[1] <= 0:self.y = 0
-        if pos[3] >= 400:self.y = 0
+        self.C.move(self.id, 0, self.dy)
+        pos = self.C.coords(self.id)
+        if pos[1] <= 0 or 400<=pos[3]:self.dy = 0
 
 # ------------- Define GUI layout -------------
 L = [[Canvas(size=(700, 400),background_color='black', key='canvas')],[Text(''), Button('Quit')]]
 W = Window('The Classic Game of Pong', L,return_keyboard_events=True, finalize=True)
 
-canvas = W['canvas'].TKCanvas
 # ------------- Create line down center, the bats and ball -------------
-canvas.create_line(350, 0, 350, 400, fill='white')
-P = pongbat(canvas, 'orange')
-Q = pongbat(canvas, 'magenta',680,660)
-B = Ball(canvas, P, Q, 'white')
+C = W['canvas'].TKCanvas
+C.create_line(350, 0, 350, 400, fill='cyan')
+P = pongbat(C, 'orange')
+Q = pongbat(C, 'magenta',680,660)
+B = Ball(C, P, Q, 'white')
 
 while 1:# ------------- Event Loop -------------
     B.draw()
     P.draw()
     Q.draw()
-
+    
     E,_= W.read(timeout=0)
     if E in(None,'Quit'):break
-    if E is not None:
-        if E.startswith('Up'):      Q.up(2)
-        elif E.startswith('Down'):  Q.down(2)
-        elif E == 'w':              P.up(1)
-        elif E == 's':              P.down(1)
+
+#    Q.mv(E.startswith('Up')and -5 or E.startswith('Down')and 5)
+#    P.mv(E == 'w' and -5 or E == 's' and 5)
+    if E.startswith('Up'):      Q.mv(-5)
+    elif E.startswith('Down'):  Q.mv(5)
+    elif E == 'w':              P.mv(-5)
+    elif E == 's':              P.mv(5)
     if B.checkwin():
         Popup('Game Over', B.checkwin() + ' won!!')
         break
-    canvas.after(10)
+    C.after(10)
 W.close()
