573631216d2d77397037eb7364a51427e5b5cb71
jamad <jamad2001@gmail.com>
2019-11-17 01:35:18 +0200

    Update Demo_Crossword_Puzzle.py

--- DemoPrograms/Demo_Crossword_Puzzle.py
+++ DemoPrograms/Demo_Crossword_Puzzle.py
@@ -1,52 +1,31 @@
-import PySimpleGUI as sg
+from PySimpleGUI import Text,Graph,Button,Window
 import random
 import string
 
-"""
-    Demo application to show how to draw rectangles and letters on a Graph Element
-    This demo mocks up a crossword puzzle board
-    It will place a letter where you click on the puzzle
-"""
+#Demo to show how to draw rectangles and letters on a Graph 
 
+w,h=5,5 # BOX_SIZE 
+n = w*h
+L = [[Text('Crossword Puzzle'), Text('', key='-OUTPUT-')],
+    [Graph((350, 350), (0, 150), (150, 0), key='-G-',change_submits=True, drag_submits=False)],
+    [Button('Show'), Button('Exit')]]
+W = Window('Window Title', L, finalize=True)
+g = W['-G-']
 
-BOX_SIZE = 25
+for i in range(h):
+    for j in range(w):
+        color=('red','white')[random.randint(0,10)>1]
+        g.draw_rectangle((j*n + 5, i*n+3),(j*n+n+5,i*n+n+3),line_color='black',fill_color=color)
+        g.draw_text(str(i*w+j+1),(j*n+10,i*n+8))
 
-layout = [
-    [sg.Text('Crossword Puzzle Using PySimpleGUI'), sg.Text('', key='-OUTPUT-')],
-    [sg.Graph((800, 800), (0, 450), (450, 0), key='-GRAPH-',
-              change_submits=True, drag_submits=False)],
-    [sg.Button('Show'), sg.Button('Exit')]
-]
-
-window = sg.Window('Window Title', layout, finalize=True)
-
-g = window['-GRAPH-']
-
-for row in range(16):
-    for col in range(16):
-        if random.randint(0, 100) > 10:
-            g.draw_rectangle((col * BOX_SIZE + 5, row * BOX_SIZE + 3), (col * BOX_SIZE + BOX_SIZE + 5, row * BOX_SIZE + BOX_SIZE + 3), line_color='black')
-        else:
-            g.draw_rectangle((col * BOX_SIZE + 5, row * BOX_SIZE + 3), (col * BOX_SIZE + BOX_SIZE + 5, row * BOX_SIZE + BOX_SIZE + 3), line_color='black', fill_color='black')
-
-        g.draw_text('{}'.format(row * 6 + col + 1),
-                    (col * BOX_SIZE + 10, row * BOX_SIZE + 8))
-
-while True:             # Event Loop
-    event, values = window.read()
-    print(event, values)
-    if event in (None, 'Exit'):
-        break
-    mouse = values['-GRAPH-']
-
-    if event == '-GRAPH-':
-        if mouse == (None, None):
-            continue
-        box_x = mouse[0]//BOX_SIZE
-        box_y = mouse[1]//BOX_SIZE
-        letter_location = (box_x * BOX_SIZE + 18, box_y * BOX_SIZE + 17)
-        print(box_x, box_y)
-        g.draw_text('{}'.format(random.choice(string.ascii_uppercase)),
-                    letter_location, font='Courier 25')
-
-window.close()
+while True:             # E Loop
+    E, V = W.read()
+    print(E, V)
+    if E in (None, 'Exit'):break
+    if E == '-G-':
+        if V['-G-']==(None,None):continue
+        x,y = V['-G-'][0]//n,V['-G-'][1]//n
+        if x<h and y<w:
+            ch=random.choice(string.ascii_uppercase)
+            g.draw_text('{}'.format(ch), (x*n+18,y*n+17), font='Courier 25')
+W.close()
