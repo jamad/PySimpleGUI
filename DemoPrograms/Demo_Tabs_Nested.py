b0529e3407cef13c829ed82b30c86438274dcf30
jamad <jamad2001@gmail.com>
2019-11-18 13:26:59 +0200

    Update Demo_Tabs_Nested.py

--- DemoPrograms/Demo_Tabs_Nested.py
+++ DemoPrograms/Demo_Tabs_Nested.py
@@ -1,39 +1,30 @@
-#!/usr/bin/env python
-import PySimpleGUI as sg
-
+from PySimpleGUI import Text,change_look_and_feel,Input,TabGroup,Tab,Output,Frame,Col,Button,Window
 # Yet another example of TabGroup element
+change_look_and_feel('GreenTan')
 
-sg.change_look_and_feel('GreenTan')
-tab2_layout = [[sg.Text('This is inside tab 2')],
-               [sg.Text('Tabs can be anywhere now!')]]
+tab1_L = [[Text('Type something here and click button'), Input(key='in')]]
+tab2_L = [[Text('This is inside tab 2')],[Text('Tabs can be anywhere now!')]]
+TG12=   TabGroup([[Tab('Tab 1', tab1_L), Tab('Tab 2', tab2_L)]])
 
-tab1_layout = [[sg.Text('Type something here and click button'), sg.Input(key='in')]]
+tab3_L = [[Text('I am tab 3')]]
+tab4_L = [[Text('I am tab 4')]]
+TG34=TabGroup([[Tab('Tab3', tab3_L), Tab('Tab 4', tab4_L)]])
 
-tab3_layout = [[sg.Text('This is inside tab 3')]]
-tab4_layout = [[sg.Text('This is inside tab 4')]]
+tab5_L = [[Text('Watch this W')] , [Output(size=(40,5))]]
 
-tab_layout = [[sg.Text('This is inside of a tab')]]
-tab_group = sg.TabGroup([[sg.Tab('Tab 7', tab_layout), sg.Tab('Tab 8', tab_layout)]])
+tab6a_L = [[Text('This is inside of a tab')]]
+tab6b_L = [[Text('This is inside of a tab')]]
+TG = TabGroup([[Tab('Tab 7', tab6a_L), Tab('Tab 8', tab6b_L)]])
+tab6_L = [[Text('This is inside tab 6')] , [Text('How about a second row of stuff in tab 6?'), TG]]
 
-tab5_layout = [[sg.Text('Watch this window')],
-                [sg.Output(size=(40,5))]]
-tab6_layout = [[sg.Text('This is inside tab 6')],
-               [sg.Text('How about a second row of stuff in tab 6?'), tab_group]]
+L = [[Frame('A Frame', layout=[[TG12, TG34]])],
+    [Text('This text is on a row with a column'),Col(layout=[[Text('In a column')],
+    [TabGroup([[Tab('Tab 5', tab5_L), Tab('Tab 6', tab6_L)]])],[Button('Click me')]])],]
 
-layout = [[sg.Text('My Window!')], [sg.Frame('A Frame', layout=
-    [[sg.TabGroup([[sg.Tab('Tab 1', tab1_layout), sg.Tab('Tab 2', tab2_layout)]]), sg.TabGroup([[sg.Tab('Tab3', tab3_layout), sg.Tab('Tab 4', tab4_layout)]])]])],
-    [sg.Text('This text is on a row with a column'),sg.Col(layout=[[sg.Text('In a column')],
-    [sg.TabGroup([[sg.Tab('Tab 5', tab5_layout), sg.Tab('Tab 6', tab6_layout)]])],
-          [sg.Button('Click me')]])],]
-
-window = sg.Window('My window with tabs', layout, default_element_size=(12,1), finalize=True)
-
+W = Window('My W with tabs', L, default_element_size=(12,1), finalize=True)
 print('Are there enough tabs for you?')
-
-while True:
-    event, values = window.read()
-    print(event, values)
-    if event is None:           # always,  always give a way out!
-        break
-
-window.close()
+while 1:
+    E, V = W.read()
+    print(E, V)
+    if E is None:break
+W.close()
